cd477113abfd0893dc7a3aa570e5e54af4ed2b8c
RD2W <mkrutovercev@yandex.ru>
2024-08-31 22:58:31 +0300

    Создать класс Navigator
    Создан класс для навигации по меню приложения и выполнения запросов от пользователя, для этой цели реализован дженерик метод navigate().

---
+++ src/main/kotlin/Navigator.kt
@@ -0,0 +1,34 @@
+class Navigator {
+    companion object {
+        fun <T> navigate(
+            title: String,
+            list: MutableList<T>,
+            onSelect: (T) -> Unit,
+            onCreate: () -> Unit
+        ) {
+            while (true) {
+                if (list.isEmpty()) {
+                    println("$title\nСписок пуст")
+                } else {
+                    println(title)
+                    list.forEachIndexed { index, item ->
+                        println("$index. $item")
+                    }
+                }
+                println("\n${list.size}. Создать\n${list.size + 1}. Выход\n\nВведите число для выбора пункта меню:")
+
+                val input = readlnOrNull()?.trim() ?: continue
+                if (!input.matches(Regex("[0-${list.size + 1}]"))) {
+                    println("Пожалуйста, введите число соответствующее пункту меню.\n")
+                    continue
+                }
+
+                when (input.toInt()) {
+                    in list.indices -> onSelect(list[input.toInt()])
+                    list.size -> onCreate()
+                    list.size + 1 -> return
+                }
+            }
+        }
+    }
+}

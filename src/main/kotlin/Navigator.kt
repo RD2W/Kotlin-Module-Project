7dafa769ed807863e580b4221cfb5ea05b501809
RD2W <mkrutovercev@yandex.ru>
2024-09-02 14:31:14 +0300

    Трансформировать класс Navigator в object Navigator

--- src/main/kotlin/Navigator.kt
+++ src/main/kotlin/Navigator.kt
@@ -1,33 +1,31 @@
-class Navigator {
-    companion object {
-        fun <T> navigate(
-            title: String,
-            list: MutableList<T>,
-            onSelect: (T) -> Unit,
-            onCreate: () -> Unit
-        ) {
-            while (true) {
-                if (list.isEmpty()) {
-                    println("$title\nСписок пуст")
-                } else {
-                    println(title)
-                    list.forEachIndexed { index, item ->
-                        println("$index. $item")
-                    }
+object Navigator {
+    fun <T> navigate(
+        title: String,
+        list: MutableList<T>,
+        onSelect: (T) -> Unit,
+        onCreate: () -> Unit
+    ) {
+        while (true) {
+            if (list.isEmpty()) {
+                println("$title\nСписок пуст")
+            } else {
+                println(title)
+                list.forEachIndexed { index, item ->
+                    println("$index. $item")
                 }
-                println("\n${list.size}. Создать\n${list.size + 1}. Выход\n\nВведите число для выбора пункта меню:")
+            }
+            println("\n${list.size}. Создать\n${list.size + 1}. Выход\n\nВведите число для выбора пункта меню:")
 
-                val input = ConsoleApp().input()
-                if (!input.matches(Regex("[0-${list.size + 1}]"))) {
-                    println("Пожалуйста, введите число соответствующее пункту меню.")
-                    continue
-                }
+            val input = ConsoleApp().input()
+            if (!input.matches(Regex("[0-${list.size + 1}]"))) {
+                println("Пожалуйста, введите число соответствующее пункту меню.")
+                continue
+            }
 
-                when (input.toInt()) {
-                    in list.indices -> onSelect(list[input.toInt()])
-                    list.size -> onCreate()
-                    list.size + 1 -> return
-                }
+            when (input.toInt()) {
+                in list.indices -> onSelect(list[input.toInt()])
+                list.size -> onCreate()
+                list.size + 1 -> return
             }
         }
     }

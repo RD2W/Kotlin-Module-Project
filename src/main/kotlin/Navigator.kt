5db6909d897415d3b4bed8b57aad1addfd0a09db
RD2W <mkrutovercev@yandex.ru>
2024-09-02 15:28:55 +0300

    Изменить подход обработки ввода из консоли при навигации в меню программы
    Рефакторин кода и уход от проверки на "регулярках", а именно использования matches(Regex("[0-${list.size + 1}]")).

--- src/main/kotlin/Navigator.kt
+++ src/main/kotlin/Navigator.kt
@@ -16,16 +16,14 @@ object Navigator {
             }
             println("\n${list.size}. Создать\n${list.size + 1}. Выход\n\nВведите число для выбора пункта меню:")
 
-            val input = ConsoleApp().input()
-            if (!input.matches(Regex("[0-${list.size + 1}]"))) {
-                println("Пожалуйста, введите число соответствующее пункту меню.")
-                continue
-            }
-
-            when (input.toInt()) {
-                in list.indices -> onSelect(list[input.toInt()])
+            when (val input = ConsoleApp().input().toIntOrNull()) {
+                in list.indices -> onSelect(list[input!!])
                 list.size -> onCreate()
                 list.size + 1 -> return
+                else -> {
+                    println("Пожалуйста, введите число соответствующее пункту меню.")
+                    continue
+                }
             }
         }
     }
